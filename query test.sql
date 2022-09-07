5901493f202b4198c6c67787bc4c0bfce4d85aa2
Guillermo Aragón <49188994+Aragui@users.noreply.github.com>
2022-09-06 22:44:31 -0500

    Add files via upload

---
+++ query test.sql
@@ -0,0 +1,6 @@
+SELECT rw.name, b.title, rt.rating, rt.rating_date from ratings rt
+INNER JOIN books b
+	ON rt.book_id = b.id
+INNER JOIN reviewers rw
+	ON rw.id = rt.reviewer_id
+ORDER BY rw.name ASC,  b.title ASC, rt.rating DESC
